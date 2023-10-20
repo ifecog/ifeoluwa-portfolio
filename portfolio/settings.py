eb62e1758353469949d06e2e30b22bcb27845c16
progIfe <ifeoluwasamson90@gmail.com>
2023-10-20 19:13:58 +0100

    Switched from SQLite to PostgreSQL Database

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME', default='mydefaultvalue'),
+        'USER': config('USER', default='mydefaultvalue'),
+        'PASSWORD': config('PASSWORD', default='mydefaultvalue'),
+        'HOST': config('HOST', default='mydefaultvalue'),
+        'PORT': config('PORT', default='mydefaultvalue'),
     }
 }
 
