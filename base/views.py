7cfe7099f92d73104554583799a1951da4a16b8c
progIfe <ifeoluwasamson90@gmail.com>
2023-10-20 08:35:48 +0100

    Syntax corrections

--- base/views.py
+++ base/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
-from .models import About
+from .models import About, Skill
 
 # Create your views here.
 
 
 def home(request):
     abouts = About.objects.all()
+    skills = Skill.objects.all()
+    
     context = {
         'abouts': abouts,
+        'skills': skills,
     }
 
     return render(request, 'pages/home.html', context)
