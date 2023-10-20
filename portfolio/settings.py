d7b84c564e2c4abe487ac78bbb4eb611d5a2000f
progIfe <ifeoluwasamson90@gmail.com>
2023-10-20 20:54:55 +0100

    Email error fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -151,7 +151,7 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
 
 # Default primary key field type
