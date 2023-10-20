7adeda4c98ef5ae6f4383a7e6db5a7b72f253f5e
progIfe <ifeoluwasamson90@gmail.com>
2023-10-20 19:34:54 +0100

    Database host bug fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': config('NAME', default='mydefaultvalue'),
         'USER': config('USER', default='mydefaultvalue'),
         'PASSWORD': config('PASSWORD', default='mydefaultvalue'),
-        'HOST': config('HOST'),
+        'HOST': 'containers-us-west-128.railway.app',
         'PORT': config('PORT', default='mydefaultvalue'),
     }
 }
