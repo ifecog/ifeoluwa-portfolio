9f49526d35a76a2761dab96665cbfb533be0a11d
progIfe <ifeoluwasamson90@gmail.com>
2023-10-21 00:00:20 +0100

    Email error fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -148,11 +148,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # DEFAULT_FROM_EMAIL = 'config('DEFAULT_FROM_EMAIL', default='')'
 # SERVER_EMAIL = config('SERVER_EMAIL', default='')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'darasimisamson90@gmail.com'
+EMAIL_HOST_PASSWORD = 'xmamjbydgmqlioet'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
 # SERVER_EMAIL = config('SERVER_EMAIL', default='')
