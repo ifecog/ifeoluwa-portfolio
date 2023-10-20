66d219c7f4088b33ae138d11738d6890db64bfa3
progIfe <ifeoluwasamson90@gmail.com>
2023-10-20 01:02:24 +0100

    About model and migrations

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import About
+from django.utils.html import format_html
 
 # Register your models here.
+
+class AboutAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.display_photo.url))
+
+    thumbnail.short_description = 'photo'
+
+    list_display = ('id', 'thumbnail', 'name', 'title')
+    list_display_links = ('name', 'thumbnail', 'title')
+
+
+
+
+admin.site.register(About, AboutAdmin)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class About(models.Model):
+    name = models.CharField(max_length=60)
+    title = models.CharField(max_length=80)
+    display_photo = models.ImageField(upload_to='display_photo/')
+    about_photo = models.ImageField(upload_to='about_photo/')
+    body = models.TextField()
+    city = models.CharField(max_length=30, default=False)
+    country = models.CharField(max_length=30, default=False)
+    phone_number = models.CharField(max_length=16, default=False)
+    email = models.EmailField(max_length=254, default=False)
+    github_link = models.URLField(max_length=100)
+    linkedin_link = models.URLField(max_length=100)
+    instagram_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'About'
+        verbose_name_plural = 'About'
+
